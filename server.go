4dbd8dbd5a3baa9e33bf456b1fd91be1c158e3a8
Alexandrov Alexandr <a.alexandrov@smart-city.com.ua>
2021-01-10 02:59:32 +0200

    add form add todo list

--- server.go
+++ server.go
@@ -4,40 +4,50 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/thedevsaddam/renderer"
+	"encoding/json"
 )
 
 var rnd *renderer.Render
+
+type DataForm struct {
+    Name string `json:"username"`
+    Email string `json:"email"`
+    Content string `json:"content"`
+}
 
 func init() {
 	opts := renderer.Options {
 		ParseGlobPattern: "./public/*.html",
 	}
-
 	rnd = renderer.New(opts)
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-    id := r.FormValue("id")
-	rnd.HTML(w, http.StatusOK, "home", struct{Data string}{Data: id})
+func addtask(w http.ResponseWriter, r *http.Request) {
+	rnd.HTML(w, http.StatusOK, "addtask", nil)
 }
 
-func about(w http.ResponseWriter, r *http.Request) {
-	rnd.HTML(w, http.StatusOK, "about", nil)
+func userData(w http.ResponseWriter, r *http.Request) {
+    username := r.PostFormValue("username")
+    email := r.PostFormValue("email")
+    content := r.PostFormValue("content")
+    if (username == "" || email == "" || content == "") {
+        http.Redirect(w, r, "/", 301)
+    }
+    dataForm := DataForm{username, email, content}
+    jsonData, err := json.Marshal(dataForm)
+    if (err != nil) {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(jsonData)
 }
-
-/* func serverData(w http.ResponseWriter, r *http.Request) {
-        var id string = r.FormValue("id")
-		fmt.Fprintf(w, "Hello World, " + "Alex \n")
-		fmt.Fprintf(w, id + "\n")
-		if (id != "") {
-		    fmt.Fprint(w, len(id))
-		}
-} */
 
 func main() {
 	mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/about", about)
+    mux.HandleFunc("/", addtask)
+    mux.HandleFunc("/userData", userData)
     port := ":8080"
     fmt.Println("Listening on port ", port)
     http.ListenAndServe(port, mux)
