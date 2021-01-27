f4c292a5f87762fcad07ef89916cc560ca636f3f
Александров Александр <alexandrov141089@gmail.com>
2021-01-27 13:48:58 +0200

    change redirect url after add new post

--- server.go
+++ server.go
@@ -179,7 +179,7 @@ func userData(w http.ResponseWriter, r *http.Request) {
             }
         }
 
-        http.Redirect(w, r, "/addPost", 301)
+        http.Redirect(w, r, "/", 301)
     }
 }
 
